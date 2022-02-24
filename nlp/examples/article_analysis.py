fa8f44580965422a5a67fb25d2ed2a630ec9d921
RonaldBuddys <ronaldmaceachern@gmail.com>
2022-02-24 21:06:06 +0000

    add example sentences, with two entities, requires are ton more work on the cleaning front

--- nlp/examples/article_analysis.py
+++ nlp/examples/article_analysis.py
@@ -5,6 +5,7 @@
 import json
 import os
 import re
+import time
 
 import numpy as np
 import pandas as pd
@@ -29,6 +30,25 @@ def remove_suffix(name, suffixes):
         name = re.sub(f" {s} .*$| {s}$", "", name)
     return name
 
+
+def get_start_end(a, b, aname="a", bname="b"):
+
+    assert len(np.intersect1d(a, b)) == 0, f"some elements found in both 'a' and 'b'"
+
+    c = np.sort(np.concatenate([a, b]))
+    is_a = np.in1d(c, a)
+
+    res = []
+    names = []
+    for i in range(len(c)-1):
+        if is_a[i] != is_a[i+1]:
+            res.append((c[i], c[i+1]))
+            if is_a[i]:
+                names.append((aname, bname))
+            else:
+                names.append((bname, aname))
+
+    return res, names
 
 if __name__ == "__main__":
 
@@ -62,11 +82,52 @@ if __name__ == "__main__":
 
     keys = list(articles.keys())
 
+    print(f"read in: {len(keys)} articles")
+
     # ---
     # read in value chain data
     # ---
 
     vc = pd.DataFrame(list(client["refinitiv"]["VCHAINS"].find(filter={})))
+
+    # ---
+    # remove names pairs that have been erroneously found in text, because of similarities in names
+    # ----
+
+    # get all names
+    all_names = np.unique(np.concatenate([v["names_in_text"]
+                                          for k, v in articles.items()]))
+    # add a dash in names to have more explicit matching (?)
+    dash_names = {n: re.sub(" ", "-", n) for n in all_names}
+
+    # for each article replace the name with the dashed name
+    # - the idea is this will help with tokenizing
+    names_in_text_dict = {}
+    for k in keys:
+        a = articles[k]
+        anames = np.array(a["names_in_text"])
+        dnames = np.array([dash_names[an] for an in anames])
+        # replace the longest matching first - to deal with
+        name_len = [len(an) for an in anames]
+        name_ord = np.argsort(name_len)[::-1]
+
+        maintext = a["maintext"]
+        name_in_article = []
+        for an in anames[name_ord]:
+            if re.search(an, maintext):
+                # get the new-names  in article
+                name_in_article.append(an)
+                maintext = re.sub(an, dash_names[an], maintext)
+            else:
+                # print(f"could not find: {an} in {k}")
+                pass
+
+        names_in_text_dict[k] = name_in_article
+
+    # keep only those articles with two or more names_in_text
+    articles = {k: articles[k] for k, v in names_in_text_dict.items() if len(v) > 1}
+
+    print(f"after removing erroneous articles, there are now: {len(articles)} articles")
 
     # ---
     # search for articles containing pair
@@ -99,6 +160,11 @@ if __name__ == "__main__":
                         "Company Name"].unique()
         # for each company, check the articles the parent name is mentioned in
         for cn in cnames:
+            # company is a supplier and customer to itself - skip
+            if pn == cn:
+                print(f"skipping {pn}|{cn}")
+                continue
+
             pair = f"{pn}|{cn}"
             for k in pv:
                 # if company is also in article store that
@@ -137,6 +203,27 @@ if __name__ == "__main__":
     title_count.sort_values("count", ascending=False, inplace=True)
 
     print(f"there are: {len(title_count)} unique titles, {len(title_count.loc[title_count['count']> 1])} occur more than once")
+
+    # ----
+    # for each company in value chain data - count of the number of articles in
+    # ----
+
+    article_count = {}
+    # NOTE: maybe reverse this operation and just cycle through articles
+    # - rather than names and articles ~ O(m*n)
+    for n in all_names:
+        for k, v in articles.items():
+            if n in v["names_in_text"]:
+                if n in article_count:
+                    article_count[n] += 1
+                else:
+                    article_count[n] = 1
+
+    print(f"there were {len(article_count)} companies found in articles")
+
+    c_count = pd.DataFrame([(k, v) for k, v in article_count.items()],
+                            columns=["name", "in_articles"])
+    c_count.sort_values("in_articles", ascending=False, inplace=True)
 
     # ---
     # get name suffixes - in an attempt to make a 'short' name that often gets referenced in article
@@ -177,6 +264,11 @@ if __name__ == "__main__":
     # HARDCODED!
     short_name_map['International Business Machines Corp'] = "IBM"
     short_name_map['News Corp'] = 'News Corp'
+    short_name_map["Amazon.com Inc"] = "Amazon"
+    short_name_map["General Electric Co"] = "GE"
+    short_name_map["Lockheed Martin Corp"] = "Lockheed"
+
+    c_count["short_name"] = c_count["name"].map(short_name_map)
 
     # TODO: should check content to determine the which (short names?) are duplicated
 
@@ -191,6 +283,9 @@ if __name__ == "__main__":
         a = articles[k]
         for n in a['names_in_text']:
             a["maintext"] = re.sub(n, short_name_map[n], a["maintext"])
+            # HARDCODE: replace ’ with ' - just a guess to try to deal with them
+            a["maintext"] = re.sub("’", "'", a["maintext"])
+
         a["short_names_in_text"] = [short_name_map[n] for n in a["names_in_text"]]
         articles[k] = a
 
@@ -200,11 +295,16 @@ if __name__ == "__main__":
     # spacy
     # ----
 
+    # TODO: perhaps want to get rid of sentences that start with \nFILE PHOTO
+    # - and end with \nFILE PHOTO
+
+
     # https://spacy.io/usage/spacy-101#annotations
 
     # requires:$ python -m spacy download en_core_web_sm
     # nlp = spacy.load("en_core_web_lg")
-    nlp = spacy.load("en_core_web_md")
+    nlp = spacy.load("en_core_web_sm")
+    # nlp = spacy.load("en_core_web_md")
 
     # https://spacy.io/usage/rule-based-matching#entityruler
     ruler = nlp.add_pipe('entity_ruler')
@@ -213,20 +313,117 @@ if __name__ == "__main__":
     ruler.add_patterns(patterns)
     # print(json.dumps(patterns, indent=4))
 
-    a = articles[keys[1]]
+    # for articles that mention supply (or similar)
+    # - found the company pairs
 
-    doc = nlp(a["maintext"])
+    # store results in a list
+    out = []
+    investigate = []
+    # increment over each of the articles that reference supply
+    for ii, k in enumerate(supply_articles):
+        if ii % 100 == 0:
+            print(f"{ii} / {len(supply_articles)}")
+        cps = [cp for cp, v in cpairs.items() if k in v]
+        text = articles[k]["maintext"]
+        doc = nlp(text)
 
-    print(a["short_names_in_text"])
-    print(json.dumps([(ent.text, ent.label_) for ent in doc.ents if ent.label_ == "ORG"], indent=4))
-    # for token in doc:
-    #     print(token.text)
-    print(a["names_in_text"])
+        sent_list = list(doc.sents)
+        sent_start = np.array([sent.start_char for sent in doc.sents])
+        sent_end = np.array([sent.end_char for sent in doc.sents])
+        # for each pair, get the short name
+        for cp in cps:
+            # long names
+            ln = cp.split("|")
+            sn = [short_name_map[l] for l in ln]
 
-    for ent in doc.ents:
-        if ent.label_ == "ORG":
-            print(ent.text, ent.start_char, ent.end_char, ent.label_)
-        # if ent.label_ == "GPE":
+            # find the locations where entity 'a' is mentioned
+            # a = [(m.start(), m.end()) for m in re.finditer(sn[0], articles[k]["maintext"])]
+
+            try:
+                # using the entity recognition
+                # a = [(ent.start_char, ent.end_char) for ent in doc.ents if ent.text == sn[0]]
+                # TODO: review if can get deal with apostrophes on entity names through spacy
+                #  i.e. identify Lockheed Martin instead Lockheed Martin's as an entity
+                # NOTE: this will sometimes not even work: got 'Lockheed' 'Martin' as to separate entities
+                a = [(ent.start_char, ent.end_char) for ent in doc.ents if re.search(f"^{sn[0]}", ent.text)]
+                a = np.array(a)
+
+                # find the locations where entity 'b' is mentioned
+                # b = [(m.start(), m.end())  for m in re.finditer(sn[1], articles[k]["maintext"])]
+
+                # using the entity recognition
+
+                # b = [(ent.start_char, ent.end_char) for ent in doc.ents if ent.text == sn[1]]
+                b = [(ent.start_char, ent.end_char) for ent in doc.ents if re.search(f"^{sn[1]}", ent.text)]
+                b = np.array(b)
+
+                # find points in the text to connect the two, via sentence
+                start_pts, names = get_start_end(a=a[:,0], b=b[:,0], aname=sn[0], bname=sn[1])
+                end_pts, names = get_start_end(a[:,1], b[:,1], aname=sn[0], bname=sn[1])
+            except Exception as e:
+                print(e)
+                investigate.append([k, sn])
+
+            assert len(start_pts) == len(end_pts), \
+                "starting points and end points not to the, expect them to be"
+
+            # get the full sentence
+            for i, sp in enumerate(start_pts):
+                nme = names[i]
+                ep = end_pts[i]
+                # find the start sentence location
+                # - by taking the start of the first entity, identify the sentences
+                # - where that is before the end and take the maximum
+                # TODO: this should be double checked/validated
+                sloc = np.argmax(sp[0] < sent_end)
+                # end sentence location
+                eloc = np.argmax(sp[1] < sent_end)
+
+                # s = sent_list[0]
+                # s.char_span(start_idx=0, end_idx=200)
+
+                # TODO: should there be a limit in the distance / number
+                #  of sentences inbetween?
+                # for now require difference to be less than equal to two
+                # NOTE: some sentence could just be \n
+                if eloc - sloc <= 3:
+                    # 'left' sentence starts
+                    left_sent_start = sent_list[sloc].start_char
+                    # take to where the first entity start
+                    left = text[left_sent_start: sp[0]]
+                    # middle is from end of first entity to start of next
+                    middle = text[ep[0]:sp[1]]
+                    # right is from end of second entity to the end of that sentence
+                    right = text[ep[1]: sent_list[eloc].end_char]
+
+                    # full_sentence = left + nme[0] + middle + nme[1] + right
+
+                    # store results in a list
+                    out.append([nme[0], nme[1], left, middle, right, k])
+
+    df = pd.DataFrame(out, columns=["entity1", "entity2", "left", "middle", "right", "article"])
+
+    df.to_csv(get_data_path("example_inputs.tsv"), sep="\t", index=False)
+
+    # doc = nlp("A complex-example,!")
+    # print([token.text for token in doc])
+    #
+    # a = articles[keys[1]]
+    # t0 = time.perf_counter()
+    # doc = nlp(a["maintext"])
+    # t1 = time.perf_counter()
+    # # will take about ~ 11min to run fo 5500 articles
+    #
+    # print(a["short_names_in_text"])
+    # print(json.dumps([(ent.text, ent.label_) for ent in doc.ents if ent.label_ == "ORG"], indent=4))
+    # # for token in doc:
+    # #     print(token.text)
+    # print(a["names_in_text"])
+    #
+    # for ent in doc.ents:
+    #     if ent.label_ == "ORG":
+    #         print(ent.text, ent.start_char, ent.end_char, ent.label_)
+    #     # if ent.label_ == "GPE":
         # if re.search("Alphabet", ent.text):
         #     print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
