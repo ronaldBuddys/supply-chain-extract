1ae8d2ad6cf3c3cbc6d02cbcdcd6736dcee3ee5d
ronaldBuddys <ronaldmaceachern@gmail.com>
2024-01-26 13:17:21 +0000

    :hammer: update get_*_path functions

--- supply_chain_extract/__init__.py
+++ supply_chain_extract/__init__.py
@@ -5,21 +5,21 @@ def get_path(*sub_dir):
     return os.path.join(os.path.dirname(__file__), *sub_dir)
 
 
-def get_parent_path():
-    return os.path.dirname(get_path())
+def get_parent_path(*sub_dir):
+    return os.path.join(os.path.dirname(get_path()), *sub_dir)
 
 
 def get_configs_path(*sub_dir):
-    return os.path.join(get_path('configs'), *sub_dir)
+    return get_parent_path("configs", *sub_dir)
 
 
 def get_data_path(*sub_dir):
-    return os.path.join(get_path('data'), *sub_dir)
+    return get_parent_path("data", *sub_dir)
 
 
 def get_image_path(*sub_dir):
-    return os.path.join(get_path('images'), *sub_dir)
+    return get_parent_path("images", *sub_dir)
 
 
 def get_plots_path(*sub_dir):
-    return os.path.join(get_path('plots'), *sub_dir)
+    return get_parent_path("plots", *sub_dir)
